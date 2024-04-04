96595d50230aeb20e79f53c85931c2843e98ef9e
netoe1 <elynetobr@outlook.com>
2024-04-04 11:09:17 -0300

    add installer.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+echo "Welcome to Ducky Installer!"
+echo "This program will install ducky as a native program."
+wait
+
+make
+sudo mv ./ducky /usr/bin/ducky
+
--- makefile
+++ makefile
@@ -3,9 +3,5 @@
 CC = gcc
 FILENAME = ducky
 
-
-
 all:
 	${CC} -o ${FILENAME} ./src/${FILENAME}.c
-clean:
-	rm -rf ./bin/*.o ./*.o
